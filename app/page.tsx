f03861671d90f058b8d14441a40343443cde2488
tufavob <paredesedgar110702@gmail.com>
2026-09-12 09:17:59 -0500

    feat: limpieza de pagina inicial y bienvenida

--- app/page.tsx
+++ app/page.tsx
@@ -7,35 +7,35 @@ export default function Home() {
         <Image
           className="dark:invert h-5 w-[100px]"
           src="/next.svg"
-          alt="Next.js logo"
+          alt="BUEN MUCHACHO"
           width={100}
           height={20}
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
+            HOLA SOY CHAVITO{" "}
             <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
+              EL MAS PERRON PARA COMER CROQUETAS
             </code>{" "}
             file.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            {" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
-              Templates
+              
             </a>{" "}
-            or the{" "}
+            {" "}
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
-              Learning
+              
             </a>{" "}
-            center.
+            
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
